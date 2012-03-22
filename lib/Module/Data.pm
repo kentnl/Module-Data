0343907deac67c567d2dc6e060fd6ed4bc80a192
Kent Fredric <kentfredric@gmail.com>
2012-03-22 15:05:12 +1300

    Basic implementation which works

--- lib/Module/Data.pm
+++ lib/Module/Data.pm
@@ -5,7 +5,19 @@ package Module::Data;
 
 # ABSTRACT: Introspect context information about modules in @INC
 
-use Moose;
+use Moo;
+use Sub::Quote;
+use Module::Runtime qw();
+use Path::Class::File;
+use Path::Class::Dir;
+
+around BUILDARGS => sub {
+  my ( $orig, $class, @args ) = @_;
+
+  unshift @args, "package" if @args % 2 == 1;
+
+  return $class->$orig(@args);
+};
 
 =head1 SYNOPSIS
 
@@ -17,10 +29,86 @@ use Moose;
 
     $d->root; # returns the root directory in @INC that 'Package::Stash' was found inside. 
 
+    # Convenient trick to discern if you're in a development environment
+
+    my $d = Module::Data->new( 'Module::Im::Developing' );
+
+    if ( -e $d->root->parent->dir('share') ) {
+        # Yep, this dir exists, so we're in a dev context.
+        # because we know in the development context all modules are in lib/*/*
+        # so if the modules are anywhere else, its not a dev context.
+        # see File::ShareDir::ProjectDistDir for more.
+    }
+
 
 =cut
 
-__PACKAGE__->meta->make_immutable;
-no Moose;
+has package => (
+  required => 1,
+  is       => 'ro',
+  isa      => quote_sub q{
+        die "given undef for 'package' , expects a Str/module name" if not defined $_[0];
+        die " ( 'package' => $_[0] ) is not a Str/module name, got a ref : " . ref $_[0] if ref $_[0];
+        Module::Runtime::check_module_name( $_[0] );
+    },
+);
+
+has _notional_name => (
+  is      => 'ro',
+  lazy    => 1,
+  default => quote_sub q{
+        return Module::Runtime::module_notional_filename( $_[0]->package );
+    },
+);
+
+has _inc_path => (
+  is      => 'ro',
+  lazy    => 1,
+  default => quote_sub q|
+        Module::Runtime::require_module( $_[0]->package );
+        return $INC{ $_[0]->_notional_name };
+    |,
+);
+
+has path => (
+  is       => 'ro',
+  lazy     => 1,
+  init_arg => undef,
+  builder  => '_build_path',
+);
+
+sub _build_path {
+  return Path::Class::Dir->new( $_[0]->_inc_path )->absolute;
+}
+
+has root => (
+  is       => 'ro',
+  lazy     => 1,
+  init_arg => undef,
+  builder  => '_build_root',
+);
+
+sub _build_root {
+  my (@bits) = split /::/, $_[0]->package;
+  $bits[-1] .= '.pm';
+  my ($path) = $_[0]->path;
+
+  # Parent ne Self is the only cross-platform way
+  # I can think of that will stop at the top of a tree
+  # as / is not applicable on windows.
+  while ( $path->parent->absolute ne $path->absolute ) {
+    if ( not $path->is_dir ) {
+      $path = $path->parent;
+      next;
+    }
+    if ( $path->file(@bits)->absolute eq $_[0]->path->absolute ) {
+      return $path->absolute;
+    }
+    $path = $path->parent;
+  }
+  return undef;
+
+}
+no Moo;
 
 1;
---
+++ t/01_basic.t
@@ -0,0 +1,45 @@
+use strict;
+use warnings;
+
+use Test::More 0.96;
+use Module::Data;
+use Test::Fatal;
+
+my $md;
+my $data;
+
+use Data::Dump qw( pp );
+
+is(
+  exception {
+    $md = Module::Data->new('Test::Fatal');
+  },
+  undef,
+  'Creating a new object for Test::Fatal introspection works'
+);
+
+is( $md->root->file( 'Test', 'Fatal.pm' )->absolute->stringify, $md->path->absolute->stringify, "root contains package" );
+
+is(
+  exception {
+    $data = {
+      _notional_name => $md->_notional_name,
+      _inc_path      => $md->_inc_path,
+      path           => $md->path->stringify,
+      package        => $md->package,
+      root           => $md->root->stringify,
+    };
+  },
+  undef,
+  'All methods work without failing'
+);
+
+note pp($data);
+note pp($md);
+
+# FILENAME: 01_basic.t
+# CREATED: 22/03/12 14:13:34 by Kent Fredric (kentnl) <kentfredric@gmail.com>
+# ABSTRACT: Basic module load and decode tests
+
+done_testing;
+
