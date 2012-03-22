4812210270550c442f24fd1cedda9fdb46a6f165
Kent Fredric <kentfredric@gmail.com>
2012-03-22 13:22:01 +1300

    Basic mock Synopsis

--- lib/Module/Data.pm
+++ lib/Module/Data.pm
@@ -3,9 +3,22 @@ use warnings;
 
 package Module::Data;
 
-# ABSTRACT:
+# ABSTRACT: Introspect context information about modules in @INC
 
 use Moose;
+
+=head1 SYNOPSIS
+
+    use Module::Data;
+
+    my $d = Module::Data->new( 'Package::Stash' );
+
+    $d->path; # returns the path to where Package::Stash was found in @INC
+
+    $d->root; # returns the root directory in @INC that 'Package::Stash' was found inside. 
+
+
+=cut
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
